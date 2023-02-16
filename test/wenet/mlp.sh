d5a45ad1d0ed4f32cc6adee59d33b77674e4f7be
sajadmanesh <sina.sajadmanesh@gmail.com>
2023-02-16 21:21:35 +0100

    update tests on wenet

--- test/wenet/mlp.sh
+++ test/wenet/mlp.sh
@@ -1,11 +1,1 @@
-python train.py mlp \
---dataset wenet \
---num_layers 3 \
---hidden_dim 16 \
---activation selu \
---optimizer adam \
---learning_rate 0.01 \
---repeats 10 \
---batch_norm True \
---epochs 100 \
---batch_size full $@
+python train.py mlp --dataset wenet --num_layers 3 --hidden_dim 16 --activation selu --optimizer adam --learning_rate 0.05 --repeats 1 --batch_norm True --epochs 100 --batch_size full $@
