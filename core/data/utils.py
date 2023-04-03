74795d243df4bc33549af93221a304c8cb5c3790
sajadmanesh <sina.sajadmanesh@gmail.com>
2023-04-03 14:55:07 +0200

    use `torch_geometric.utils.is_torch_sparse_tensor` for num_edges

--- core/data/utils.py
+++ core/data/utils.py
@@ -1,8 +1,8 @@
-from torch import Tensor
 from torch_geometric.data import Data
+from torch_geometric.utils import is_torch_sparse_tensor
 
 def num_edges(data: Data) -> int:
-    if hasattr(data, 'adj_t') and isinstance(data.adj_t, Tensor) and (data.adj_t.is_sparse or data.adj_t.is_sparse_csr):
+    if hasattr(data, 'adj_t') and is_torch_sparse_tensor(data.adj_t):
         return data.adj_t._nnz()
     else:
         return data.num_edges
