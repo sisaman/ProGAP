9959bede2af17c6ae600551e9311ea0e98e19540
sajadmanesh <sina.sajadmanesh@gmail.com>
2023-04-26 11:52:06 +0200

    temporary commit

--- core/trainer/trainer.py
+++ core/trainer/trainer.py
@@ -1,13 +1,15 @@
 from copy import deepcopy
 import torch
-from torch.types import Number
+# from torch.types import Number
 from torch.optim import Optimizer
 from typing import Annotated, Iterable, Literal, Optional
 from core.args.utils import ArgInfo
-from core.loggers.factory import Logger
-from torchmetrics import MeanMetric
+from core.trainer.checkpoint import InMemoryCheckpoint
+# from torchmetrics import MeanMetric
 from core.trainer.progress import TrainerProgress
 from core.modules.base import Metrics, Phase, TrainableModule
+from lightning import Trainer as LightningTrainer
+from lightning.pytorch.loggers import Logger
 
 
 class Trainer:
@@ -16,7 +18,8 @@ class Trainer:
                  monitor:       str = 'val/acc',
                  monitor_mode:  Literal['min', 'max'] = 'max',
                  val_interval:  Annotated[int, ArgInfo(help='interval of validation')] = 1,
-                 verbose:       Annotated[bool, ArgInfo(help='display progress')] = False,
+                 verbose:       Annotated[bool, ArgInfo(help='display progress')] = True,
+                 logger:        Optional[Logger] = None,
                  ):
 
         assert monitor_mode in ['min', 'max']
@@ -26,42 +29,47 @@ class Trainer:
         self.monitor = monitor
         self.monitor_mode = monitor_mode
         self.verbose = verbose
+        self.logger = logger
         
         # trainer internal state
-        self.model: TrainableModule = None
-        self.metrics: dict[str, MeanMetric] = {}
+        # self.model: TrainableModule = None
+        # self.metrics: dict[str, MeanMetric] = {}
+
+        self.reset()
 
     def reset(self):
-        self.model = None
-        self.metrics = {}
+        # self.model = None
+        # self.metrics = {}
+        self.trainer = None
+        
 
-    def update_metrics(self, metric_name: str, metric_value: object, weight: int = 1) -> None:
-        # if this is a new metric, add it to self.metrics
-        device = metric_value.device if torch.is_tensor(metric_value) else 'cpu'
-        if metric_name not in self.metrics:
-            self.metrics[metric_name] = MeanMetric(compute_on_step=False).to(device)
+    # def update_metrics(self, metric_name: str, metric_value: object, weight: int = 1) -> None:
+    #     # if this is a new metric, add it to self.metrics
+    #     device = metric_value.device if torch.is_tensor(metric_value) else 'cpu'
+    #     if metric_name not in self.metrics:
+    #         self.metrics[metric_name] = MeanMetric(compute_on_step=False).to(device)
 
-        # update the metric
-        self.metrics[metric_name].update(metric_value, weight=weight)
+    #     # update the metric
+    #     self.metrics[metric_name].update(metric_value, weight=weight)
 
-    def aggregate_metrics(self, phase: Phase='train') -> Metrics:
-        metrics = {}
+    # def aggregate_metrics(self, phase: Phase='train') -> Metrics:
+    #     metrics = {}
 
-        for metric_name, metric_value in self.metrics.items():
-            if phase in metric_name.split('/'):
-                value = metric_value.compute()
-                metric_value.reset()
-                metrics[metric_name] = value
+    #     for metric_name, metric_value in self.metrics.items():
+    #         if phase in metric_name.split('/'):
+    #             value = metric_value.compute()
+    #             metric_value.reset()
+    #             metrics[metric_name] = value
 
-        return metrics
+    #     return metrics
 
-    def is_better(self, current_metric: Number, previous_metric: Number) -> bool:
-        if self.monitor_mode == 'max':
-            return current_metric > previous_metric
-        elif self.monitor_mode == 'min':
-            return current_metric < previous_metric
-        else:
-            raise ValueError(f'Unknown metric mode: {self.monitor_mode}')
+    # def is_better(self, current_metric: Number, previous_metric: Number) -> bool:
+    #     if self.monitor_mode == 'max':
+    #         return current_metric > previous_metric
+    #     elif self.monitor_mode == 'min':
+    #         return current_metric < previous_metric
+    #     else:
+    #         raise ValueError(f'Unknown metric mode: {self.monitor_mode}')
 
     def fit(self, 
             model: TrainableModule, 
@@ -70,69 +78,54 @@ class Trainer:
             train_dataloader: Iterable, 
             val_dataloader: Optional[Iterable]=None, 
             test_dataloader: Optional[Iterable]=None, 
-            checkpoint: bool=False,
             ) -> Metrics:
 
         self.model = model
         self.optimizer = optimizer
-        monitor_key = f'{self.monitor}'
 
-        if checkpoint:
-            best_state_dict = None
+        progress = TrainerProgress(
+            # num_epochs=epochs, 
+            # num_train_steps=len(train_dataloader), 
+            # num_val_steps=len(val_dataloader), 
+            # num_test_steps=len(test_dataloader),
+            # disable=not self.verbose,
+        )
 
-        if val_dataloader is None:
-            val_dataloader = []
+        checkpoint = InMemoryCheckpoint(
+            monitor=self.monitor, 
+            mode=self.monitor_mode
+        )
 
-        if test_dataloader is None:
-            test_dataloader = []
+        self.trainer = LightningTrainer(
+            accelerator='auto',
+            callbacks=[progress, checkpoint],
+            logger=self.logger,
+            max_epochs=epochs,
+            val_check_interval=self.val_interval,
+            check_val_every_n_epoch=None,
+            log_every_n_steps=1,
+            enable_checkpointing=False,
+            enable_progress_bar=self.verbose,
+            enable_model_summary=False,
+            deterministic=True,
+        )
 
-        self.progress = TrainerProgress(
-            num_epochs=epochs, 
-            num_train_steps=len(train_dataloader), 
-            num_val_steps=len(val_dataloader), 
-            num_test_steps=len(test_dataloader),
-            disable=not self.verbose,
+        self.trainer.fit(
+            model=model,
+            train_dataloaders=train_dataloader,
+            val_dataloaders=val_dataloader,
+            test_dataloaders=test_dataloader,
         )
         
-        with self.progress:
-            best_metrics = None
-            num_epochs_without_improvement = 0
-            
-            for epoch in range(1, epochs + 1):
-                metrics = {f'epoch': epoch}
+        
+        # test loop
+        if test_dataloader:
+            test_metrics = self.loop(test_dataloader, phase='test')
+            metrics.update(test_metrics)
 
-                # train loop
-                train_metrics = self.loop(train_dataloader, phase='train')
-                metrics.update(train_metrics)
-                    
-                # validation loop
-                if val_dataloader and self.val_interval and epoch % self.val_interval == 0:
-
-                    val_metrics = self.loop(val_dataloader, phase='val')
-                    metrics.update(val_metrics)
-
-                    if best_metrics is None or self.is_better(metrics[monitor_key], best_metrics[monitor_key]):
-                        best_metrics = metrics
-                        num_epochs_without_improvement = 0
-
-                        if checkpoint:
-                            best_state_dict = deepcopy(self.model.state_dict())
-                    else:
-                        num_epochs_without_improvement += 1
-                        if num_epochs_without_improvement >= self.patience > 0:
-                            break
-
-                # test loop
-                if test_dataloader:
-                    test_metrics = self.loop(test_dataloader, phase='test')
-                    metrics.update(test_metrics)
-
-                # log and update progress
-                Logger.get_instance().log(metrics)
-                self.progress.update(task='epoch', metrics=metrics, advance=1)
-
-        if best_metrics is None:
-            best_metrics = metrics
+        
+        if checkpoint.best_metrics is None:
+            best_metrics = 
         else:
             # load best model if checkpointing is enabled
             if checkpoint and best_state_dict is not None:
---
+++ pyg.py
@@ -0,0 +1,113 @@
+import time
+import os.path as osp
+
+import lightning.pytorch as pl
+import torch
+import torch.nn.functional as F
+from torch.nn import BatchNorm1d
+from torchmetrics import Accuracy
+
+from torch_geometric.data.lightning import LightningNodeData
+from torch_geometric.datasets import Reddit
+from torch_geometric.nn import GraphSAGE
+from lightning.pytorch.callbacks import ModelCheckpoint, RichProgressBar
+from lightning.pytorch.strategies import SingleDeviceStrategy
+from core.datasets.loader import DatasetLoader
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.plugins import CheckpointIO
+from core.data.loader.node import NodeDataLoader
+
+
+class Model(pl.LightningModule):
+    def __init__(self, in_channels: int, out_channels: int,
+                 hidden_channels: int = 256, num_layers: int = 2,
+                 dropout: float = 0.5):
+        super().__init__()
+        self.gnn = GraphSAGE(in_channels, hidden_channels, num_layers,
+                             out_channels, dropout=dropout,
+                             norm=BatchNorm1d(hidden_channels))
+
+        self.train_acc = Accuracy(task='multiclass', num_classes=out_channels)
+        self.val_acc = Accuracy(task='multiclass', num_classes=out_channels)
+        self.test_acc = Accuracy(task='multiclass', num_classes=out_channels)
+        self.final_acc = Accuracy(task='multiclass', num_classes=out_channels)
+
+    def forward(self, x, adj_t):
+        return self.gnn(x, adj_t)
+
+    def training_step(self, data, batch_idx):
+        y_hat = self(data.x, data.adj_t)[data.train_mask]
+        y = data.y[data.train_mask]
+        loss = F.cross_entropy(y_hat, y)
+        self.train_acc(y_hat.softmax(dim=-1), y)
+        self.log('train_acc', self.train_acc, prog_bar=True, on_step=False,
+                 on_epoch=True)
+        return loss
+
+    def validation_step(self, data, batch_idx, dataloader_idx=0):
+        if dataloader_idx == 0:
+            metric = 'val_acc'
+            acc = self.val_acc
+        else:
+            metric = 'test_acc'
+            acc = self.test_acc
+            
+        y_hat = self(data.x, data.adj_t)[data.val_mask]
+        y = data.y[data.val_mask]
+        acc(y_hat.softmax(dim=-1), y)
+        self.log(metric, acc, prog_bar=True, on_step=False,
+                 on_epoch=True, add_dataloader_idx=False)
+
+    def test_step(self, data, batch_idx):
+        y_hat = self(data.x, data.adj_t)[data.test_mask]
+        y = data.y[data.test_mask]
+        self.final_acc(y_hat.softmax(dim=-1), y)
+        self.log('final_test_acc', self.final_acc, prog_bar=True, on_step=False,
+                 on_epoch=True)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.01)
+
+
+class InMemoryCheckpoint(CheckpointIO):
+    def __init__(self) -> None:
+        super().__init__()
+        self.checkpoint_dict = {}
+
+    def load_checkpoint(self, path, map_location = None):
+        return self.checkpoint_dict[path]
+    
+    def save_checkpoint(self, checkpoint, path, storage_options = None):
+        self.checkpoint_dict[path] = checkpoint
+
+    def remove_checkpoint(self, path):
+        del self.checkpoint_dict[path]
+
+
+if __name__ == '__main__':
+    pl.seed_everything(12345)
+
+    data = DatasetLoader('facebook').load()
+
+    train_dataloader = NodeDataLoader(data=data, phase='train')
+    val_dataloader = NodeDataLoader(data=data, phase='val')
+    test_dataloader = NodeDataLoader(data=data, phase='test')
+    final_dataloader = NodeDataLoader(data=data, phase='test')
+
+    num_classes = data.y.max().item() + 1
+    model = Model(data.num_features, num_classes)
+    checkpoint = ModelCheckpoint(monitor='val_acc', save_top_k=1, mode='max', dirpath='checkpoints')
+    # logger = WandbLogger()
+
+    trainer = pl.Trainer(devices=1, max_epochs=5, callbacks=[checkpoint, RichProgressBar()], deterministic=True, accelerator='cpu',
+                        #  logger=WandbLogger(), 
+                         plugins=InMemoryCheckpoint(), 
+                         log_every_n_steps=1)
+
+    start = time.time()
+    trainer.fit(model, train_dataloaders=train_dataloader, val_dataloaders=[val_dataloader, test_dataloader])
+    trainer.test(ckpt_path='best', dataloaders=test_dataloader)
+    end = time.time()
+    duration = end - start
+    # logger.log_metrics({'duration': duration})
+    print(checkpoint.best_model_path)
